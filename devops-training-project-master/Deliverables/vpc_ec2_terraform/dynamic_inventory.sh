dc242b275f4f80105d0b72a36ecf286ae2683148
Ronak Sharma <Ronaksharmaapril30@gmail.com>
2022-09-03 14:10:23 +0530

    Update dynamic_inventory.sh

--- devops-training-project-master/Deliverables/vpc_ec2_terraform/dynamic_inventory.sh
+++ devops-training-project-master/Deliverables/vpc_ec2_terraform/dynamic_inventory.sh
@@ -1,6 +1,6 @@
 PUB_IP=`aws ec2 describe-instances --filters Name=tag:Name,Values=tf-instance --region ap-southeast-1 | grep "PublicIpAddress" | awk '{print $2}' | sed 's/"//g' | sed 's/,//g'`
 echo $PUB_IP
-ANS=/var/lib/jenkins/workspace/terraform-ansible-prov-conf/Deliverables/ansible-plays/
+ANS=/var/lib/jenkins/workspace/terraform-ansible-prov-conf/devops-training-project-master/Deliverables/ansible-plays/
 echo 'creating inventory.ini...' > $ANS/inventory.ini
 cat <<EOF > $ANS/inventory.ini
 [all:vars]
